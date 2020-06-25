d45ac8cd6d0526cece2239fa34215f8409838b11
Jordan <jordandavid253@gmail.com>
2020-06-25 18:07:13 +0000

    add license and comments.

--- portfolio/src/main/webapp/quiz.js
+++ portfolio/src/main/webapp/quiz.js
@@ -1,3 +1,21 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Prompts the user with a quiz.
+ */
+
 function quiz(){
     var totalscore = 6;
     let myinformation = {};
@@ -7,6 +25,8 @@ function quiz(){
     myinformation["second favorite sport"] = "wrestling";
     myinformation["favorite hobby"] = "working out";
     myinformation["favorite city"] = "San Francisco";
+
+    //prompt user with a question
     var userscore = 0;
     for (let info in myinformation){
         let useranswer = prompt("What is my " + info + "?", "");
@@ -14,6 +34,8 @@ function quiz(){
             userscore++;
         }  
     }
+
+    //display final score
     alert("Your score is " + 100*userscore/totalscore + "%");
 }
 
