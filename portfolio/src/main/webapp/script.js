8f4b70420c49263bcaa17731dcf5dc9b24b4ebc0
Jordan <jordandavid253@gmail.com>
2020-06-26 18:01:46 +0000

    Use async and await keywords to request '/data'.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,10 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * The async and await keywords use fetch to request '/data' and add it to DOM.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+ async function getDataUsingAsyncAwait() {
+     const response = await fetch('/data');
+     const quote = await response.text();
+     document.getElementById('data-container').innerText = quote;
+ }
